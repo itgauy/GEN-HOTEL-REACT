aebbd53e8e62df803dc8b0bef7ba56561b757f5d
lash0000 <k80308392@gmail.com>
2025-02-11 00:02:30 +0800

    project added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+      <div>
+        <h1>I was a stupid!</h1>
+        <Button>
+          adsfkoasdfokdsakfo
+        </Button>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
