89127fdf3be2e27c474a5a0ed7af4c8218f9edba
lash0000 <k80308392@gmail.com>
2025-04-26 02:12:12 +0800

    kms for torreslian

---
+++ src/users/administrators/knowledge/pages/Contacts.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Cardboard from "../components/Cardboard";
+
+function Hotel_Services_Contacts() {
+    return (
+        <section id="welcome-page" className="p-8">
+            <div>
+                <h1 className="text-2xl font-semibold mb-4">Page 4</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Hotel_Services_Contacts;
--- src/users/administrators/knowledge/routes/KnowledgeAdmin.jsx
+++ src/users/administrators/knowledge/routes/KnowledgeAdmin.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
 import Knowledge_Appbar from '../layouts/Appbar';
-import Knowledge_Page from '../pages/MainPage';
+import Knowledge_Page from '../pages/Overview';
 import { RequiredAuth } from '@/lib/Auth';
+import Content_Management_Page from '../pages/ContentManagement';
+import Article_Management from '../pages/ArticleManagement';
+import Hotel_Services_Contacts from '../pages/Contacts';
 
 const KnowledgeAdminRoutes = () => [
   <Route
@@ -15,6 +18,9 @@ const KnowledgeAdminRoutes = () => [
     }
   >
     <Route index element={<Knowledge_Page />} />
+    <Route path="/kms-admin/cms-portal" element={<Content_Management_Page />} />
+    <Route path="/kms-admin/article-management" element={<Article_Management />} />
+    <Route path="/kms-admin/contact-list" element={<Hotel_Services_Contacts />} />
   </Route>
 ]
 
